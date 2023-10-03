f86582c2cd0167036e63195a5b2bbee0cfbc3569
Shina <53410646+s7tya@users.noreply.github.com>
2023-10-04 08:13:06 +0900

    add .min.json

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod kdb;
 
 use anyhow::Result;
-use std::path::Path;
+use std::{path::Path, sync::Arc};
 
 fn main() -> Result<()> {
+    tracing_subscriber::fmt().init();
+
     let csv_file_path = Path::new("dist/kdb.csv");
     if !csv_file_path.exists() {
-        let client = ureq::Agent::new();
+        let client = Arc::new(ureq::Agent::new());
         let url = kdb::grant_session(&client);
         let url = kdb::search_courses(&client, url);
 
@@ -14,8 +16,10 @@ fn main() -> Result<()> {
     }
 
     let output_file_path = Path::new("dist/kdb.json");
-
     let _ = kdb::convert_courses_csv_to_json(csv_file_path, output_file_path, true)?;
+
+    let output_min_file_path = Path::new("dist/kdb.min.json");
+    let _ = kdb::convert_courses_csv_to_json(csv_file_path, output_min_file_path, false);
 
     Ok(())
 }
