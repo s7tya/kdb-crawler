8fae7ba0e93ca76f1ee505ee8e979c984a6fcb5c
Shina <53410646+s7tya@users.noreply.github.com>
2024-04-01 15:40:51 +0900

    update to 2024

--- src/kdb.rs
+++ src/kdb.rs
@@ -9,7 +9,7 @@ use std::{
 use ureq::Agent;
 
 const KDB_URL: &str = "https://kdb.tsukuba.ac.jp";
-const YEAR: i32 = 2023;
+const YEAR: i32 = 2024;
 
 pub fn grant_session(client: &Agent) -> String {
     let res = client.get(KDB_URL).call();
